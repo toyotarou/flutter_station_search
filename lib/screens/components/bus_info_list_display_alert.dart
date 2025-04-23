e4c3eb9ce09891708e613335444410df7970885f
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-04-24 07:49:06 +0900

    commit 20250424

--- lib/screens/components/bus_info_list_display_alert.dart
+++ lib/screens/components/bus_info_list_display_alert.dart
@@ -4,6 +4,7 @@ import 'package:latlong2/latlong.dart';
 
 import '../../controllers/tokyo_train/tokyo_train.dart';
 import '../../extensions/extensions.dart';
+import '../../models/station_extends_model.dart';
 import '../../models/tokyo_station_model.dart';
 import '../../utility/utility.dart';
 
@@ -44,9 +45,11 @@ class _BusInfoListDisplayAlertState extends ConsumerState<BusInfoListDisplayAler
       roopData = <String>['no bus data'];
     }
 
+    final List<StationExtendsModel> busStationList = <StationExtendsModel>[];
+
     for (final String element in roopData) {
       if (tokyoStationTokyoStationModelListMap[element] != null) {
-        var distance = utility.calcDistance(
+        final String distance = utility.calcDistance(
           originLat: widget.selectedStationLatLng.latitude,
           originLng: widget.selectedStationLatLng.longitude,
           destLat: tokyoStationTokyoStationModelListMap[element]![0].lat.toDouble(),
@@ -54,29 +57,46 @@ class _BusInfoListDisplayAlertState extends ConsumerState<BusInfoListDisplayAler
         );
 
         if (distance != '') {
-          list.add(
-            DefaultTextStyle(
-              style: const TextStyle(fontSize: 12),
-              child: Container(
-                padding: const EdgeInsets.all(5),
-                margin: const EdgeInsets.symmetric(vertical: 1),
-                decoration: BoxDecoration(
-                  border: Border(bottom: BorderSide(color: Colors.white.withOpacity(0.3))),
-                  color: Colors.black.withOpacity(0.3),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(element),
-                    Text(distance.toDouble().toStringAsFixed(2)),
-                  ],
-                ),
-              ),
+          busStationList.add(
+            StationExtendsModel(
+              id: 0,
+              stationName: tokyoStationTokyoStationModelListMap[element]![0].stationName,
+              address: tokyoStationTokyoStationModelListMap[element]![0].address,
+              lat: tokyoStationTokyoStationModelListMap[element]![0].lat,
+              lng: tokyoStationTokyoStationModelListMap[element]![0].lng,
+              lineNumber: '',
+              lineName: '',
+              distance: distance.toDouble(),
             ),
           );
         }
       }
     }
+
+    busStationList
+      ..sort((StationExtendsModel a, StationExtendsModel b) => a.distance.compareTo(b.distance) * -1)
+      ..forEach((StationExtendsModel element) {
+        list.add(
+          DefaultTextStyle(
+            style: const TextStyle(fontSize: 12),
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.symmetric(vertical: 1),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.white.withOpacity(0.3))),
+                color: Colors.black.withOpacity(0.3),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(element.stationName),
+                  Text(element.distance.toStringAsFixed(2)),
+                ],
+              ),
+            ),
+          ),
+        );
+      });
 
     return SingleChildScrollView(child: Column(children: list));
   }
